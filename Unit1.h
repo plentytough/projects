5d7c7036e2549168847ec75b8d9b3f4de7a609d2
Kenji Hirose <139457384+plentytough@users.noreply.github.com>
2023-08-08 12:33:01 +0900

    Add files via upload

---
+++ Unit1.h
@@ -0,0 +1,78 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+
+
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <System.Bindings.Expression.hpp>
+#include <System.Bindings.Helper.hpp>
+#include <Data.DB.hpp>
+#include <FireDAC.Comp.Client.hpp>
+#include <FireDAC.Phys.hpp>
+#include <FireDAC.Phys.IB.hpp>
+#include <FireDAC.Phys.IBDef.hpp>
+#include <FireDAC.Phys.Intf.hpp>
+#include <FireDAC.Stan.Async.hpp>
+#include <FireDAC.Stan.Def.hpp>
+#include <FireDAC.Stan.Error.hpp>
+#include <FireDAC.Stan.Intf.hpp>
+#include <FireDAC.Stan.Option.hpp>
+#include <FireDAC.Stan.Pool.hpp>
+#include <FireDAC.UI.Intf.hpp>
+#include <FireDAC.VCLUI.Wait.hpp>
+#include <FireDAC.Comp.DataSet.hpp>
+#include <FireDAC.DApt.hpp>
+#include <FireDAC.DApt.Intf.hpp>
+#include <FireDAC.DatS.hpp>
+#include <FireDAC.Stan.Param.hpp>
+
+
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE で管理されるコンポーネント
+	TEdit *Edit1;
+	TLabel *Label1;
+	TLabel *Label2;
+	TButton *Button1;
+	TLabel *Label3;
+	TLabel *Label4;
+	TLabel *Label5;
+	TLabel *Label6;
+	TLabel *Label7;
+	TLabel *Label8;
+	TLabel *Label9;
+	TLabel *Label10;
+	TLabel *Label11;
+	TLabel *Label12;
+	TLabel *Label13;
+	TLabel *Label14;
+	TLabel *Label15;
+	TLabel *Label16;
+	TLabel *Label17;
+	TLabel *Label18;
+	TLabel *Label19;
+	TLabel *Label20;
+	TButton *Button2;
+	TButton *Button3;
+	TFDConnection *FDConnection1;
+	TFDTransaction *FDTransaction1;
+	TFDQuery *FDQuery1;
+	void __fastcall Button2Click(TObject *Sender);
+	void __fastcall Button1Click(TObject *Sender);
+	void __fastcall Button3Click(TObject *Sender);
+	void __fastcall FormCreate(TObject *Sender);
+
+private:	// ユーザー宣言
+public:		// ユーザー宣言
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
